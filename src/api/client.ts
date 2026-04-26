8457329f4ab920f87f28b7ae55a35807db67b91f
kuNA78 <tagoreacademy123@gmail.com>
2026-04-26 11:27:15 +0530

    Add authorization token to API requests

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,10 +1,10 @@
-
 import axios from 'axios';
 import { store } from '../store/store';
 
 const api = axios.create({
   baseURL: 'https://ecommmm-gsre.onrender.com/api',
 });
+
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
