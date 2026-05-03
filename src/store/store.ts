beda8dc7e121ce58835ea3159c82ef9a482120b2
kuNA78 <tagoreacademy123@gmail.com>
2026-05-04 00:13:26 +0530

    Refactor store.ts for improved type usage

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,18 +1,17 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { useSelector } from "react-redux";
+import { configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import authReducer from '../features/auth/authSlice';
 import cartReducer from '../features/cart/cartSlice';
 
 export const store = configureStore({
-    reducer: {
-        auth: authReducer,
-        cart: cartReducer,
-    },
+  reducer: {
+    auth: authReducer,
+    cart: cartReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-export const useAppSelector = <TSelected>(
-    selector: (state: RootState) => TSelected
-): TSelected => useSelector(selector);
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
